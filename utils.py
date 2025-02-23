c1a3d647947d5b309d54512f2e69ddbb9913eb72
Jabali Shah <45881375+jshah88@users.noreply.github.com>
2025-02-23 00:51:25 -0500

    Loading Env in Config

--- utils.py
+++ utils.py
@@ -8,8 +8,33 @@ import os
 
 def load_config(config_path="config.yaml"):
     """Load YAML configuration file."""
-    with open(config_path, "r") as f:
-        return yaml.safe_load(f)
+    config = {'region': 'us-east-1',
+     'sagemaker_execution_role': 'arn:aws:iam::701586278703:role/service-role/AmazonSageMaker-ExecutionRole-20250214T203663',
+     'ecr_repository': 'flight-delay-prediction-xgboost',
+     'sagemaker_endpoint_name': 'flight-delay-xgboost-endpoint-single-request',
+     'bucket': 'sagemaker-us-east-1-701586278703',
+     's3_csv_private_path': 's3://sagemaker-us-east-1-701586278703/airline-delay-cause/csv/',
+     'athena_database': 'db_airline_delay_cause', 'database_name': 'db_airline_delay_cause',
+     'raw_table_name': 'airline_delay_cause_csv_raw',
+     's3_staging_dir': 's3://sagemaker-us-east-1-701586278703/athena/staging', 'dev_table_name': 'development_data',
+     'prod_table_name': 'production_data', 's3_prefix': 'flight-delay-prediction-xgboost',
+     'feature_store_s3_dev': 's3://sagemaker-us-east-1-701586278703/feature-store/dev/',
+     'feature_store_s3_prod': 's3://sagemaker-us-east-1-701586278703/feature-store/prod/',
+     'dev_s3_uri': 's3://sagemaker-us-east-1-701586278703/feature-store/dev/',
+     'prod_s3_uri': 's3://sagemaker-us-east-1-701586278703/feature-store/prod/',
+     'dev_fg_name': 'airline_delay_features_dev', 'prod_fg_name': 'airline_delay_features_prod',
+     'baseline_model_metrics_json': 's3://sagemaker-us-east-1-701586278703/models/model_metrics.json',
+     'baseline_model_pkl': 's3://sagemaker-us-east-1-701586278703/models/linear_regression_model.pkl',
+     'xgboost_model': 's3://sagemaker-us-east-1-701586278703/xgboost_models/xgboost_model.tar.gz',
+     'xgboost_model_metrics_json': 's3://sagemaker-us-east-1-701586278703/xgboost_models/model_metrics.json',
+     'training': {'num_round': 100, 'max_depth': 5, 'eta': 0.2, 'subsample': 0.8, 'eval_metric': 'auc'},
+     'monitoring': {'instance_type': 'ml.m5.xlarge', 'instance_count': 1, 'max_runtime_in_seconds': 3600,
+                    'schedule': 'hourly'}}
+    return config
+    # with open(config_path, "r") as f:
+    #     config=yaml.safe_load(f)
+    #     print(config)
+    #     return yaml.safe_load(f)
 
 def init_aws_session(region=None):
     """Initialize and return a boto3 session and SageMaker session."""
